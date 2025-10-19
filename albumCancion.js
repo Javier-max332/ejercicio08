8fe2b1a2e26227cb075c18074203b0ca7af01ece
Javier Guadalupe Torres Viramontes <mt8966902@gmail.com>
2025-10-18 22:18:26 -0600

    Arreglo objetos de canciones y la función que coloca las URL

---
+++ albumCancion.js
@@ -0,0 +1,277 @@
+const albums = [
+    {
+        album: "Revolver",
+        anio: 1966,
+        descografica: "Parlophone",
+        numCanciones: "14",
+        canciones: [
+            {
+                nombre: "Taxman",
+                url: "https://www.youtube.com/embed/DS-SAIqBfUY?si=EVOJTXRm8ubcNn_l",
+                duracion: "2:39",
+                compositores: "George Harrison"
+            },
+            {
+                nombre: "Eleanor Rigby",
+                url: "https://www.youtube.com/embed/HuS5NuXRb5Y",
+                duracion: "2:08",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "I'm Only Sleeping",
+                url: "https://www.youtube.com/embed/L-OEnyBwHrE?si=ltV8eCKem1AES2D7",
+                duracion: "3:01",
+                compositores: "Lennon, McCartney"  
+            },
+            {
+                nombre: "Love You To",
+                url: "https://www.youtube.com/embed/tE93TJDKTQU",
+                duracion: "3:00",
+                compositores: "George Harrison"
+            },
+            {
+                nombre: "Here, There and Everywhere",
+                url: "https://www.youtube.com/embed/vs7U4xfkAfI",
+                duracion: "2:25",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Yellow Submarine",
+                url: "https://www.youtube.com/embed/m2uTFF_3MaA",
+                duracion: "2:40",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "She Said She Said",
+                url: "https://www.youtube.com/embed/3iybCwJdudM",
+                duracion: "2:37",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Good Day Sunshine",
+                url: "https://www.youtube.com/embed/6dC7ILQ_vtE",
+                duracion: "2:09",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "And Your Bird Can Sing",
+                url: "https://www.youtube.com/embed/usNsCeOV4GM",
+                duracion: "2:01",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "For No One",
+                url: "https://www.youtube.com/embed/mQER0A0ej0M",
+                duracion: "2:01",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Doctor Robert",
+                url: "https://www.youtube.com/embed/YR5ApYxkU-U",
+                duracion: "2:14",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "I Want to Tell You",
+                url: "https://www.youtube.com/embed/yqUUBUbfK3U",
+                duracion: "2:30",
+                compositores: "George Harrison"
+            },
+            {
+                nombre: "Got to Get You into My Life",
+                url: "https://www.youtube.com/embed/azvE5dh2f1A",
+                duracion: "2:29",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Tomorrow Never Knows",
+                url: "https://www.youtube.com/embed/6a3NcwfOBzQ",
+                duracion: "2:57",
+                compositores: "Lennon, McCartney"
+            }
+        ]
+    },
+    {
+        album: "Magical Mystery Tour",
+        anio: 1967,
+        descografica: "Parlophone",
+        numCanciones: "11",
+        canciones: [
+            {
+                nombre: "Magical Mystery Tour",
+                url: "https://www.youtube.com/embed/qNWrqNDYdxI",
+                duracion: "2:51",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "The Fool on the Hill",
+                url: "https://www.youtube.com/embed/wsRatIMUSu8?si=XTgDcKHjOEWK6Tmi",
+                duracion: "3:00",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Flying",
+                url: "https://www.youtube.com/embed/3XzvYkWbZjk",
+                duracion: "2:17",
+                compositores: "Lennon, McCartney, Harrison, Starr"
+            },
+            {
+                nombre: "Blue Jay Way",
+                url: "https://www.youtube.com/embed/BJ0xBCwkg3E",
+                duracion: "3:56",
+                compositores: "George Harrison"
+            },
+            {
+                nombre: "Your Mother Should Know",
+                url: "https://www.youtube.com/embed/NWc3D2H6xlg",
+                duracion: "2:29",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "I Am the Walrus",
+                url: "https://www.youtube.com/embed/ES5nN8D1GYA",
+                duracion: "4:35",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Hello, Goodbye",
+                url: "https://www.youtube.com/embed/rblYSKz_VnI",
+                duracion: "3:27",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Strawberry Fields Forever",
+                url: "https://www.youtube.com/embed/HtUH9z_Oey8",
+                duracion: "4:07",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Penny Lane",
+                url: "https://www.youtube.com/embed/S-rB0pHI9fU",
+                duracion: "3:03",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Baby You're a Rich Man",
+                url: "https://www.youtube.com/embed/if7c1a3ubL8",
+                duracion: "3:00",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "All You Need Is Love",
+                url: "https://www.youtube.com/embed/_7xMfIp-irg",
+                duracion: "3:57",
+                compositores: "Lennon, McCartney"
+            }
+        ]
+    },
+    {
+        album: "Yellow Submarine",
+        anio: 1969,
+        descografica: "Apple Records",
+        numCanciones: "13",
+        canciones: [
+            {
+                nombre: "Yellow Submarine",
+                url: "https://www.youtube.com/embed/m2uTFF_3MaA",
+                duracion: "2:40",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Only a Northern Song",
+                url: "https://www.youtube.com/embed/7uY0Ab5HlZ0",
+                duracion: "3:24",
+                compositores: "George Harrison"
+            },
+            {
+                nombre: "All Together Now",
+                url: "https://www.youtube.com/embed/h4QEZ-1X5z0",
+                duracion: "2:09",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Hey Bulldog",
+                url: "https://www.youtube.com/embed/BlO2ZxrVRB0",
+                duracion: "3:12",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "It's All Too Much",
+                url: "https://www.youtube.com/embed/26A-49WbqgI",
+                duracion: "6:25",
+                compositores: "George Harrison"
+            },
+            {
+                nombre: "All You Need Is Love",
+                url: "https://www.youtube.com/embed/_7xMfIp-irg",
+                duracion: "3:57",
+                compositores: "Lennon, McCartney"
+            },
+            {
+                nombre: "Pepperland",
+                url: "https://www.youtube.com/embed/0W7N5JcnHk4",
+                duracion: "2:21",
+                compositores: "George Martin"
+            },
+            {
+                nombre: "Sea of Time",
+                url: "https://www.youtube.com/embed/0nJxqQbQf4E",
+                duracion: "2:18",
+                compositores: "George Martin"
+            },
+            {
+                nombre: "Sea of Holes",
+                url: "https://www.youtube.com/embed/7fQpV9qP-TU",
+                duracion: "2:16",
+                compositores: "George Martin"
+            },
+            {
+                nombre: "Sea of Monsters",
+                url: "https://www.youtube.com/embed/Kb8y5FmuLWg",
+                duracion: "3:36",
+                compositores: "George Martin"
+            },
+            {
+                nombre: "March of the Meanies",
+                url: "https://www.youtube.com/embed/RM_1A2x5NLM",
+                duracion: "2:18",
+                compositores: "George Martin"
+            },
+            {
+                nombre: "Pepperland Laid Waste",
+                url: "https://www.youtube.com/embed/8R-R2W1kShE",
+                duracion: "2:13",
+                compositores: "George Martin"
+            },
+            {
+                nombre: "Yellow Submarine in Pepperland",
+                url: "https://www.youtube.com/embed/foYbB0AsoWg",
+                duracion: "2:13",
+                compositores: "George Martin"
+            }
+        ]
+    }
+];
+
+        function mostrarCancion(albumNombre, numCancion) {
+            //se busca el álbum por su nombre
+            const album = albums.find(a => a.album === albumNombre);
+
+            //ya teniendo el álbum donde encontrarémos la canción, ahora vamos a 
+            //sacar la canción que se encuentra en ese álbum por num de posición
+            const cancion = album.canciones[numCancion];
+
+            //asiganmos la URL al iframe para mostrar la canción.
+
+            //encontrar a cual lista ordenada le dio clic.
+            document.addEventListener("click", (event) => {
+                if (event.target.closest("#album-revolver")) {
+                    document.getElementById("videoPlayer_Revolver").src = cancion.url;
+                } else if (event.target.closest("#album-magicalMysteryTour")) {
+                    document.getElementById("videoPlayer_magical").src = cancion.url;
+                } else if (event.target.closest("#album-yellowSubmarine")) {
+                    document.getElementById("videoPlayer_Yellow").src = cancion.url;
+                }
+            });     
+            console.log(`Reproduciendo: ${cancion.nombre} (${album.album})`);
+        }
